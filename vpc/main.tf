f8f83ea43b3c964a0e35d81f6955102724bce0c1
chieftonyit <chieftonyit1985@gmail.com>
2025-02-09 18:44:58 -0600

    First

---
+++ vpc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "tykama.jfrog.io/tf__clouds/vpc/local"
+  version = "v1.0.2"
+
+  private_cidr = var.private_cidr
+  availability_zones = var.availability_zones
+  public_cidr = var.public_cidr
+  environment = var.environment
+  cidr_block = var.cidr_block
+  log_destination = var.log_destination
+  log_destination_type = var.log_destination_type
+  vpc_name = var.vpc_name
+  region = var.region
+  
+}
